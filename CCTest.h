93149e07c0ffba8aaa92e26b9c52f9a4c5471830
Peng Zhou <statchou@gmail.com>
2014-09-08 17:14:44 -0400

    add a test class:

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Peng Zhou on 9/8/14.
+//  Copyright (c) 2014 CodePeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Peng Zhou on 9/8/14.
+//  Copyright (c) 2014 CodePeng. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
